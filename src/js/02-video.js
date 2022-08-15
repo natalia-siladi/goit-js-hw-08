795578e133ca12e09eff477135b96ba06a3f5171
Natalia-siladi <vladsil2k19@gmail.com>
2022-08-15 12:47:31 +0300

    добавляет правки

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,19 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', Throttle(onTimeupdate, 1000));
+function onTimeupdate(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
 
 let currentTime = localStorage.getItem('videoplayer-current-time');
+player.on('timeupdate', Throttle(onTimeupdate, 1000));
+
+// const startVideoPosition = localStorage.getItem('videoplayer-current-time') || '0'
 if (currentTime) {
     player.setCurrentTime(currentTime);
 }
 
 
-function onTimeupdate(seconds) {
-    localStorage.setItem('videoplayer-current-time', seconds);
-}
+
 
 
