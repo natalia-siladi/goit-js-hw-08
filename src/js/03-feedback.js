685ea5b72c518016af3ab423eceff4989bf6dae3
Natalia-siladi <vladsil2k19@gmail.com>
2022-08-19 18:17:28 +0300

    ДОБАВЛЯЕТ ПРАВКИ

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,25 +10,14 @@ const refs = {
 };
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', Throttle(onMessageInput, 500));
-// refs.message.addEventListener('input', Throttle(onMessageInput, 500));
-// refs.email.addEventListener('input', Throttle(onEmailInput, 500));
 refs.form.addEventListener('input', e => {
     formData[e.target.name] = e.target.value;
 
 });
 
-
-
 populateMessage();
 let formFeedbak = {}
 
-function onFormSubmit(evt) {
-    evt.preventDefault()
-    evt.currentTarget.reset();
-    console.log(formData)
-    localStorage.removeItem('feedback - form - state')
-
-};
 
 function onMessageInput(evt) {
     formFeedbak = {
@@ -39,28 +28,25 @@ function onMessageInput(evt) {
     localStorage.setItem('feedback - form - state', JSON.stringify(formFeedbak));
 };
 
-// function onMessageInput(evt) {
-//     const message = evt.target.value;
-//     localStorage.setItem('feedback - form - state', message);
-// };
-// function onEmailInput(evt) {
-//     const email = evt.target.value;
-//     localStorage.setItem('feedback - form - state', email);
-// };
-// function populateMessage() {
-//     const savedMessage = localStorage.getItem('feedback - form - state');
-//     if (savedMessage) {
-// refs.message.value = savedMessage;
-//     }
-// }
-
 function populateMessage() {
-    const savedMessage = localStorage.getItem('feedback - form - state');
-    if (savedMessage) {
-        refs.form.elements.email.value = savedMessage.email;
-        refs.form.elements.message.value = savedMessage.message;
-    };
+    const initValues = localStorage.getItem('feedback - form - state');
+    const newValues = JSON.parse(initValues);
+
+    if (newValues) {
+        refs.form.elements.email.value = newValues.email;
+
+
+        refs.form.elements.message.value = newValues.message;
+
+    }
 }
+function onFormSubmit(evt) {
+    evt.preventDefault()
+    evt.currentTarget.reset();
+    console.log(formData)
+    localStorage.removeItem('feedback - form - state')
+
+};
 
 
 
